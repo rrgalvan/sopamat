85e0e1ffecd4d8c67bb22a35c83c26048881264c
Rafa Rodríguez Galván <rafa.rodriguez.galvan@gmail.com>
2016-05-24 23:42:11 +0200

    Redesigned finite diference example. cpu_time=23.3s, gpu_time=2.98s !

--- openacc/FDJacobi/finite_diff_1d.f90
+++ openacc/FDJacobi/finite_diff_1d.f90
@@ -1,3 +1,5 @@
+! Running: ./finite_diff_1d (maybe with ACC_NOTIFY=1)
+
 module double
   integer, parameter :: dp = kind(1.0d0) ! double precission
 end module double
@@ -12,38 +14,38 @@ contains
   pure function f(x)
     real(dp), intent(in) :: x
     real(dp) :: f
-    f = -12*x*x
+    f = 2 !-12*x*x
   end function f
 
-  function finite_diff_1d(a, b, alpha, beta, npoints ) result(u)
+  function finite_diff_1d(a, b, alpha, beta, n ) result(u)
     real(dp), intent(in) :: a, b, alpha, beta !! Interval pts & boundary conditions
-    integer, intent(in) :: npoints ! Number of interior points
-    real(dp), dimension(0:npoints+1) :: u
-    real(dp), dimension(1:npoints) :: new_u
+    integer, intent(in) :: n ! Number of interior points
+    real(dp), dimension(0:n+1) :: u
+    real(dp), dimension(1:n) :: new_u
     integer :: iter, i
-    integer, parameter :: niters = 5 ! Number of time iterations
+    integer, parameter :: niters = 10000 ! Number of time iterations
     real(dp) :: x_i, hx
 
     ! Set up data
-    hx = (b-a)/(npoints+2)
-    do i = 0, npoints+1
-       u(i) = alpha + (beta-alpha)*i/(npoints+1)
+    hx = (b-a)/(n+2)
+    do i = 0, n+1
+       u(i) = alpha + (beta-alpha)*i/(n+1)
     enddo
 
     call acc_init( acc_device_nvidia )
-    !$acc data copy(u(:),new_u(:))
+    !acc data copyin(u), create(new_u)
+    !$acc data copy(u, new_u)
     do iter = 1,niters
-       !acc kernels loop
-       do i = 1, npoints
+       !$acc kernels loop
+       do i = 1, n
           x_i = a + hx*i
           ! print *, x_i, f(x_i)
-          new_u(i) = 0.5_dp*( u(i-1) + u(i+1) + hx*hx*f(x_i) ) ! 12*x_i*x_i )
+          new_u(i) = 0.5_dp*( u(i-1) + u(i+1) + hx*hx*2 ) !hx*hx*f(x_i) )
        enddo
-       print *, iter, new_u
-       !acc kernels loop
-       do i = 1, npoints
+       !$acc kernels loop
+       do i = 1,n
           u(i) = new_u(i)
-       enddo
+       end do
     enddo
     !$acc end data
   end function finite_diff_1d
@@ -54,20 +56,18 @@ program test
   use diff_finitas
   implicit none
 
-  integer, parameter :: npoints=10
-  real(dp), dimension(0:npoints+1) :: u
+  integer, parameter :: n=1000000
+  real(dp), dimension(0:n+1) :: u
   integer::i
   real t0, t1
 
   call cpu_time(t0)
-  u = finite_diff_1d(0._dp, 1._dp, 0._dp, 1._dp, npoints)
+  u = finite_diff_1d(-1._dp, 1._dp, 0._dp, 0._dp, n)
   call cpu_time(t1)
-  if(.true.) then
+  if(.false.) then
      print *, "Result:"
-     do i = 0, npoints+1, npoints/20
-        write(*, '(g8.3)', advance='no') u(i)
-     enddo
-     print *
+     print *, u
   end if
+  print *, "max=", maxval(u)
   print '("Time = ", g8.3," seconds")', t1-t0
 end program test
